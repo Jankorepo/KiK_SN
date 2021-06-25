6ae41fc105e367759ee2c8330277c16eab706a3c
Jankorepo <44462192+Jankorepo@users.noreply.github.com>
2021-06-25 11:10:24 +0200

    Add cloning a web

--- Neuron.cs
+++ Neuron.cs
@@ -7,6 +7,16 @@ namespace KiK_SN
     class Neuron
     {
         double output;
-        List<double> matrix;
+        List<double> matrix=new List<double>();
+        public Neuron Copy(Neuron my_old_neuron)
+        {
+            Neuron my_new_neuron = new Neuron();
+            foreach (var number in my_old_neuron.matrix)
+            {
+                my_new_neuron.matrix.Add(number);
+            }
+            my_new_neuron.output = my_old_neuron.output;
+            return my_old_neuron;
+        }
     }
 }
--- Web.cs
+++ Web.cs
@@ -6,5 +6,22 @@ namespace KiK_SN
 {
     class Web
     {
+        public List<List<Neuron>> layers;
+        public List<double> web_structure;
+        public Web Copy(Web my_old_web)
+        {
+            Web my_new_web = new Web();
+            my_new_web.layers = new List<List<Neuron>>();
+            foreach (var layer in my_old_web.layers)
+            {
+                my_new_web.layers.Add(new List<Neuron>());
+                foreach (var single_neuron in layer)
+                {
+                    my_new_web.layers[my_new_web.layers.Count - 1].Add(single_neuron.Copy(single_neuron));
+                }
+            }
+            my_new_web.web_structure = my_old_web.web_structure;
+            return my_new_web;
+        }
     }
 }
