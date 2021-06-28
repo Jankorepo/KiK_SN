8b86caab10c9b0e9765e816dc9ac77b2c436291e
Jankorepo <44462192+Jankorepo@users.noreply.github.com>
2021-06-28 18:34:21 +0200

    Connect web to TTT game and small fixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,27 +24,61 @@ namespace KiK_SN
     public partial class MainWindow : Window
     {
         Web neural_web = new Web();
+        TicTacToe board = new TicTacToe();
         public MainWindow()
         {
             InitializeComponent();
-            neural_web.web_structure = new List<int>() { 3,3,2,2 };
+            neural_web.web_structure = new List<int>() { 9,5,9 };
             neural_web.Fill(neural_web);
         }
 
         private void Button01_Click(object sender, RoutedEventArgs e)
         {
-            /*
-            List<List<List<double>>> lista = JsonConvert.DeserializeObject<List<List<List<double>>>>(File.ReadAllText(@"train_data/sumatorek.json"));
-            //neural_web.FillSetValues(neural_web); // sprawdzenie czy sieć działa na przygotowanym wcześniej przykładzie
-            for (int i = 0; i < 100000; i++)
+            int epochs = 1000000;
+            int games_to_leave = 0;
+            List<double> licz = new List<double>() { 0,0,0 };
+            for (int i = 0; i < epochs; i++)
             {
-                List<List<double>> random_data_to_learn = lista[new Random().Next(lista.Count)];
-                CalculateWebData.Output(neural_web, random_data_to_learn[0]);
-                CalculateWebData.BackwardPropagation(neural_web, random_data_to_learn[1], 0.1);
-                neural_web.Clean();
+                List<List<List<Double>>> saved_moves = board.PlayASingleGame(neural_web);
+                double winner = saved_moves[2][0][0];
+                PrepareSavedMovesToLearn(saved_moves, winner);
+                for (int j= 0; j < saved_moves[0].Count; j++)
+                {
+                    CalculateWebData.Output(neural_web, saved_moves[1][j]);
+                    CalculateWebData.BackwardPropagation(neural_web, saved_moves[0][j], 0.1);
+                    neural_web.Clean();
+                }
+                if (winner == 2)
+                    licz[2]++;
+                else if (winner == 1)
+                    licz[1]++;
+                else if (winner == 0)
+                    licz[0]++;
+                else if (winner == 5)
+                    games_to_leave++;
             }
-            */
+            MessageBox.Show("Sieć wygrała "+ Math.Round((licz[2] / (epochs - games_to_leave)), 4)*100 + "% gier"+"\n"+
+            "Losowość wygrała " + Math.Round((licz[1] /( epochs - games_to_leave)), 4) * 100 + "% gier" + "\n" +
+            "Równość wygrała " + Math.Round((licz[0] / (epochs - games_to_leave)), 4) * 100 + "% gier" + "\n");
+        }
 
+        private void PrepareSavedMovesToLearn(List<List<List<double>>> saved_moves, double winner)
+        {
+            List<List<Double>> web_moves = saved_moves[0];
+            if(winner==2)
+                ChooseMoveToChange(web_moves, 1);
+            else if(winner == 1 || winner ==5)
+                ChooseMoveToChange(web_moves, 0);
+            else if (winner == 0)
+                ChooseMoveToChange(web_moves, 0.95);
+        }
+
+        private void ChooseMoveToChange(List<List<double>> web_moves, double value)
+        {
+            foreach (var single_move in web_moves)
+                for (int i = 0; i < single_move.Count; i++)
+                    if (single_move.Max() == single_move[i])
+                        single_move[i] = value;
         }
     }
 }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -7,7 +7,14 @@ namespace KiK_SN
 {
     class TicTacToe
     {
-        List<double> game_board = new List<double>();
+        public List<double> game_board = new List<double>() { 0,0,0,0,0,0,0,0,0 };
+        private List<double> CloneBoard(List<double> old_board)
+        {
+            List<Double> new_board = new List<double>();
+            foreach (var board_value in old_board)
+                new_board.Add(board_value);
+            return new_board;
+        }
         public int GameResult()
         {
             int X_win = 2;
@@ -16,7 +23,7 @@ namespace KiK_SN
                 return 2;
             else if (CheckResultAtThisMoment(Y_win) == true)
                 return 1;
-            else if (!game_board.Contains(0))
+            else if (game_board.Contains(0))
                 return 0;
             return -1;
         }
@@ -25,7 +32,7 @@ namespace KiK_SN
             for (int i = 0; i < 3; i++)
                 if (value == game_board[i] && value == game_board[i+1] && value == game_board[i+2])
                     return true;
-            for (int i = 0; i < 3; i = i++)
+            for (int i = 0; i < 3; i++)
                 if (value == game_board[i] && value == game_board[i+3] && value == game_board[i+6])
                     return true;
             if (value == game_board[0] && value == game_board[4] && value == game_board[8] || 
@@ -45,13 +52,69 @@ namespace KiK_SN
                     game_board[i] = which_player;
                     break;
                 }  
-                else if (game_board[i] < 0)
+                else if (game_board[i] > 0)
                     choosen_field++;
             }
         }
         public void Move(int which_player, int field)
         {
             game_board[field] = which_player;
+        }
+        public List<List<List<double>>> PlayASingleGame(Web neural_web)
+        {
+            int result = 0;
+            int current_player = 2;
+            List<List<double>> saved_web_moves = new List<List<double>>();
+            List<List<double>> saved_board_moves = new List<List<double>>();
+            List<List<double>> winner = new List<List<double>>();
+            while (result == 0)
+            {
+                if (current_player == 2)
+                {
+                    saved_board_moves.Add(CloneBoard(game_board));
+                    current_player = 1;
+                    CalculateWebData.Output(neural_web, game_board);
+                    double best_value=neural_web.layers[neural_web.layers.Count - 1].Max(neuron => neuron.output);
+                    int field = FindListIndex(best_value, neural_web.layers[neural_web.layers.Count - 1]);
+                    saved_web_moves.Add(GetWebOutputValues(neural_web.layers[neural_web.layers.Count - 1]));
+                    if (game_board[field] != 0)
+                        result = 5;
+                    else
+                        Move(2, field);
+                }
+                else if (current_player == 1)
+                {
+                    RandomMove(current_player);
+                    current_player = 2;
+                }
+                if(result!=5)
+                    result = GameResult();
+            }
+            if (result == 5)
+                winner.Add(new List<double>() { 5 });
+            else if (result == 2)
+                winner.Add(new List<double>() { 2 });
+            else if (result == 1)
+                winner.Add(new List<double>() { 1 });
+            else if (result == -1)
+                winner.Add(new List<double>() { 0 });
+            
+            game_board = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            return new List<List<List<double>>>() { saved_web_moves, saved_board_moves, winner };
+        }
+        private int FindListIndex(double best_value, List<Neuron> list_of_outputs)
+        {
+            for (int i = 0; i < list_of_outputs.Count; i++)
+                if (best_value == list_of_outputs[i].output)
+                    return i;
+            return 0;
+        }
+        private List<double> GetWebOutputValues(List<Neuron> neuron_list)
+        {
+            List<double> values = new List<double>();
+            foreach (var neuron in neuron_list)
+                values.Add(neuron.output);
+            return values;
         }
     }
 }
